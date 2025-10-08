6b89754b4020bc176c86c027b26346251b137387
Your Name <you@example.com>
2025-10-07 20:27:05 -0600

    primer punto de control

---
+++ mi-portafolio/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+// src/app/app.routes.ts
+import { Routes } from '@angular/router';
+
+// Importa los componentes que creaste
+import { Inicio } from './pages/inicio/inicio';
+import { SobreMi } from './pages/sobre-mi/sobre-mi';
+import { Proyectos } from './pages/proyectos/proyectos';
+import { Experiencia } from './pages/experiencia/experiencia';
+import { Contacto } from './pages/contacto/contacto';
+
+export const routes: Routes = [
+    { path: 'inicio', component: Inicio },
+    { path: 'sobre-mi', component: SobreMi },
+    { path: 'proyectos', component: Proyectos },
+    { path: 'experiencia', component: Experiencia },
+    { path: 'contacto', component: Contacto },
+    { path: '', redirectTo: '/inicio', pathMatch: 'full' }, // Ruta por defecto
+    { path: '**', redirectTo: '/inicio' } // Cualquier otra ruta, redirige al inicio
+];
